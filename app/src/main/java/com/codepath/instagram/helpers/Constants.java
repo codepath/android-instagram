f1466eb2e515d1547af3bfa0b266ac522233302d
Nick Aiwazian <nickaiwazian@gmail.com>
2015-11-28 22:49:39 -0800

    Update Instagram permission scope

--- app/src/main/java/com/codepath/instagram/helpers/Constants.java
+++ app/src/main/java/com/codepath/instagram/helpers/Constants.java
@@ -2,5 +2,5 @@ package com.codepath.instagram.helpers;
 
 public class Constants {
     public static final String REDIRECT_URI = "oauth://codepath.com";
-    public static final String SCOPE = "comments";
+    public static final String SCOPE = "basic";
 }
